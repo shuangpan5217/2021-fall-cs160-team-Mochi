5a30a16f0936b55013842038ba06d76ae8aae2e6
aprilchao <aprilemeraldchao@gmail.com>
2021-09-26 23:56:02 -0700

    adding api call for feasibility prototype

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,18 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = e => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    console.log(username, password);
+
+    await fetch('http://localhost:3000/v1/login?signup=true', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username,
+        password
+      })
+  });
+
   };
 
   return (
@@ -15,7 +24,7 @@ function App() {
       <main className="form-signup">
         <form onSubmit={handleSubmit}>
             <h1 className="h3 mb-3 fw-normal">Please sign up</h1>
-            <input type ="username" className="form-control" placeholder="name@example.com" required
+            <input type ="username" className="form-control" placeholder="Username" required
                    onChange={e => setUsername(e.target.value)}
             />
             <input type="password" className="form-control" placeholder="Password" required
