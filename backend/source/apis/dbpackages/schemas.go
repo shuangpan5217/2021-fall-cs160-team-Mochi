085ec5b9ee6d8c9ea86f7a927e8c1df14d8f844a
shp076 <span5217@gmail.com>
2021-10-09 20:48:50 -0700

    modify schema

--- backend/source/apis/dbpackages/schemas.go
+++ backend/source/apis/dbpackages/schemas.go
@@ -20,15 +20,21 @@ const (
 
 // create foreign key constraints
 const (
-	// users and groups
+	// users and groups access
 	group_users_username_fk = `ALTER TABLE group_users DROP CONSTRAINT IF EXISTS group_user_username_fk; ALTER TABLE group_users ADD CONSTRAINT group_user_username_fk FOREIGN KEY (username) REFERENCES Users(username) ON DELETE CASCADE;`
 	group_users_groupID_fk  = `ALTER TABLE group_users DROP CONSTRAINT IF EXISTS group_users_groupID_fk; ALTER TABLE group_users ADD CONSTRAINT group_users_groupID_fk FOREIGN KEY (group_id) REFERENCES Groups(group_id) ON DELETE CASCADE;`
 
-	// users and notes
+	// users and groups owner fk
+	group_users_groupOwner_fk = `ALTER TABLE groups DROP CONSTRAINT IF EXISTS group_users_groupOwner_fk; ALTER TABLE groups ADD CONSTRAINT group_users_groupOwner_fk FOREIGN KEY (group_owner) REFERENCES Users(username) ON DELETE CASCADE;`
+
+	// users and notes owner fk
+	user_notes_noteOwner_fk = `ALTER TABLE notes DROP CONSTRAINT IF EXISTS user_notes_noteOwner_fk; ALTER TABLE notes ADD CONSTRAINT user_notes_noteOwner_fk FOREIGN KEY (note_owner) REFERENCES Users(username) ON DELETE CASCADE;`
+
+	// users and notes access
 	user_notes_username_fk = `ALTER TABLE user_notes DROP CONSTRAINT IF EXISTS user_notes_username_fk; ALTER TABLE user_notes ADD CONSTRAINT user_notes_username_fk FOREIGN KEY (username) REFERENCES Users(username) ON DELETE CASCADE;`
 	user_notes_noteID_fk   = `ALTER TABLE user_notes DROP CONSTRAINT IF EXISTS user_notes_noteID_fk; ALTER TABLE user_notes ADD CONSTRAINT user_notes_noteID_fk FOREIGN KEY (note_id) REFERENCES Notes(note_id) ON DELETE CASCADE;`
 
-	// groups and notes
+	// groups and notes access
 	group_notes_groupID_fk = `ALTER TABLE group_notes DROP CONSTRAINT IF EXISTS group_notes_groupID_fk; ALTER TABLE group_notes ADD CONSTRAINT group_notes_groupID_fk FOREIGN KEY (group_id) REFERENCES Groups(group_id) ON DELETE CASCADE;`
 	group_notes_noteID_fk  = `ALTER TABLE group_notes DROP CONSTRAINT IF EXISTS group_notes_noteID_fk; ALTER TABLE group_notes ADD CONSTRAINT group_notes_noteID_fk FOREIGN KEY (note_id) REFERENCES Notes(note_id) ON DELETE CASCADE;`
 
@@ -49,57 +55,69 @@ type User struct {
 }
 
 type Group struct {
-	GroupID     uuid.UUID `gorm:"primary_key;not null" json:"group_id"`
-	GroupName   string    `gorm:"not null" json:"group_name"` // unique for the same user
-	Description string    `json:"description"`
-	GroupOwner  string    `gorm:"not null" json:"group_owner"` // username of the user
+	GroupID     uuid.UUID  `gorm:"primary_key;not null" json:"group_id"`
+	GroupName   string     `gorm:"not null" json:"group_name"` // unique for the same user
+	Description string     `json:"description"`
+	GroupOwner  string     `gorm:"index;not null" json:"group_owner"` // username of the user.
+	CreatedAt   *time.Time `gorm:"default:CURRENT_TIMESTAMP;" json:"created_at"`
+	UpdatedAt   *time.Time `gorm:"default:CURRENT_TIMESTAMP;" json:"updated_at"`
 }
 
 type GroupUser struct {
-	GroupID  uuid.UUID `gorm:"not null" json:"group_id"` // once the group is deleted, all related group users will be deleted
-	Username string    `gorm:"not null" json:"username"` // once the user is deleted, all related group users will be deleted
+	GroupID  uuid.UUID `gorm:"index;not null" json:"group_id"` // once the group is deleted, all related group users will be deleted
+	Username string    `gorm:"index;not null" json:"username"` // once the user is deleted, all related group users will be deleted
 }
 
 type Note struct {
-	NoteID        uuid.UUID `gorm:"primary_key;not null" json:"note_id"`
-	NoteOwner     string    `gorm:"not null" json:"note_owner"` // username of note owner. Once delted, all notes will be deleted
-	Description   string    `json:"description"`
-	Title         string    `json:"title"`
-	Category      string    `json:"category"`
-	NoteReference string    `gorm:"not null" json:"note_reference"`
-	Type          string    `json:"type"`
-	Tag           string    `gorm:"not null" json:"tag"`
+	NoteID        uuid.UUID  `gorm:"primary_key;not null" json:"note_id"`
+	NoteOwner     string     `gorm:"index;not null" json:"note_owner"` // username of note owner. Once deleted, all notes will be deleted
+	Description   string     `json:"description"`
+	Title         string     `json:"title"`
+	Category      string     `json:"category"`
+	NoteReference string     `gorm:"not null" json:"note_reference"`
+	Type          string     `json:"type"`
+	Tag           string     `gorm:"not null" json:"tag"`
+	CreatedAt     *time.Time `gorm:"default:CURRENT_TIMESTAMP;" json:"created_at"`
+	UpdatedAt     *time.Time `gorm:"default:CURRENT_TIMESTAMP;" json:"updated_at"`
 }
 
 // users have access to notes
 type UserNote struct {
-	Username string    `gorm:"not null" json:"username"`
-	NoteID   uuid.UUID `gorm:"not null" json:"note_id"`
+	Username string    `gorm:"index;not null" json:"username"`
+	NoteID   uuid.UUID `gorm:"index;not null" json:"note_id"`
 }
 
 // groups have access to notes
 type GroupNote struct {
-	GroupID uuid.UUID `gorm:"not null" json:"group_id"`
-	NoteID  uuid.UUID `gorm:"not null" json:"note_id"`
+	GroupID uuid.UUID `gorm:"index;not null" json:"group_id"`
+	NoteID  uuid.UUID `gorm:"index;not null" json:"note_id"`
 }
 
 type Comment struct {
-	CommentID uuid.UUID `gorm:"primary_key;not null" json:"comment_id"`
-	NoteID    uuid.UUID `gorm:"not null" json:"note_id"` // once deleted, all comments will be deleted
-	Username  string    `gorm:"not null" json:"username"`
-	Content   string    `gorm:"not null" json:"content"`
+	CommentID uuid.UUID  `gorm:"primary_key;not null" json:"comment_id"`
+	NoteID    uuid.UUID  `gorm:"index;not null" json:"note_id"` // once deleted, all comments will be deleted
+	Username  string     `gorm:"not null" json:"username"`
+	Content   string     `gorm:"not null" json:"content"`
+	CreatedAt *time.Time `gorm:"default:CURRENT_TIMESTAMP;" json:"created_at"`
+	UpdatedAt *time.Time `gorm:"default:CURRENT_TIMESTAMP;" json:"updated_at"`
 }
 
 func CreateFKConstraints(db *gorm.DB) {
-	// users and groups
+	// users and groups access
 	db.Exec(group_users_username_fk)
 	db.Exec(group_users_groupID_fk)
 
-	// users and notes
+	// users and groups owner fk
+	db.Exec(group_users_groupOwner_fk)
+
+	// users and notes owner fk
+	db.Exec(user_notes_noteOwner_fk)
+
+	// users and notes access
 	db.Exec(user_notes_username_fk)
 	db.Exec(user_notes_noteID_fk)
 
-	// groups and notes
+	// groups and notes access
 	db.Exec(group_notes_groupID_fk)
 	db.Exec(group_notes_noteID_fk)
 
