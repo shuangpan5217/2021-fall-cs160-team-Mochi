01cf65e578fa6676d9bcb6f37b053bd0235d61e0
aprilchao <aprilemeraldchao@gmail.com>
2021-10-10 13:21:59 -0700

    relinked login functionality

--- frontend/src/components/inputBox.jsx
+++ frontend/src/components/inputBox.jsx
@@ -1,9 +1,14 @@
 import "../css/forms.css";
 
-function InputBox({ placeholder }) {
+function InputBox({ placeholder, onChange }) {
   return (
     <>
-      <input type="text" className="agenda text-input-box" placeholder={placeholder} />
+      <input
+        type="text"
+        className="agenda text-input-box"
+        placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+      />
     </>
   );
 }
--- frontend/src/components/loginWindow.jsx
+++ frontend/src/components/loginWindow.jsx
@@ -1,13 +1,40 @@
 import Button from "./button";
 import InputBox from "./inputBox";
 import ModalHeader from "./modalHeader";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 function LoginWindow(props) {
+  const history = useHistory();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const attemptLogin = async () => {
+    const response = await fetch("http://localhost:3000/v1/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const responseJSON = await response.json();
+    if (!responseJSON.status_code) {
+      history.push("/home");
+    } else if (responseJSON.status_code === 401) {
+      alert("Incorrect username or password.");
+    } else {
+      alert("Something went wrong");
+    }
+  };
+
   return (
     <div className="d-flex flex-column align-items-center">
       <ModalHeader title="Welcome to MochiNote!" />
-      <InputBox placeholder="username" />
-      <InputBox placeholder="password" />
+      <InputBox placeholder="username" onChange={setUsername} />
+      <InputBox placeholder="password" onChange={setPassword} />
       <Button
         title="forgot your password?"
         type="link"
@@ -15,7 +42,7 @@ function LoginWindow(props) {
       />
       <div className="d-flex flex-row">
         <Button title="SIGN UP" type="secondary" url="/signup" />
-        <Button title="LOG IN" type="primary" url="/home" />
+        <Button title="LOG IN" type="primary" clicked={attemptLogin} />
       </div>
     </div>
   );
