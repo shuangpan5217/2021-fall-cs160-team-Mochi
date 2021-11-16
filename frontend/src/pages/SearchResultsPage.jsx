b5cf738f381bc3aee92e4902d11792119b619cd9
aprilchao <aprilemeraldchao@gmail.com>
2021-11-15 18:55:59 -0800

    finished laying out ui for search results page

--- frontend/src/pages/SearchResultsPage.jsx
+++ frontend/src/pages/SearchResultsPage.jsx
@@ -1,11 +1,14 @@
 import Template from "../components/template";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import SectionTitle from "../components/sectionTitle";
 import AppContext from "../components/AppContext";
+import PDFViewer from "../components/PDFViewer";
+import mockPDF from "../media/mockPDF.pdf";
 import "../css/searchResultsPage.css";
 
 function SearchResultsPage(props) {
     const myContext = useContext(AppContext);
+    const [thumbnails, setThumbnails] = useState([]);
 
     useEffect(() => {
         // return () => {
@@ -13,6 +16,12 @@ function SearchResultsPage(props) {
         //     myContext.setGlobalQuery("");
         // };
         //this code is breaking the search results page, need to find a different solution in a future PR
+        const numThumbnails = 20;
+        let tempArr = [];
+        for (let i = 0; i < numThumbnails; i++) {
+            tempArr.push(mockPDF);
+        }
+        setThumbnails(tempArr);
     }, []);
 
     return (
@@ -23,8 +32,13 @@ function SearchResultsPage(props) {
                 body={
                     <div className="d-flex flex-column align-items-start search-results-container">
                         <SectionTitle title="Search Results (20+)" />
-                        <p>Filter: {myContext.filter}</p>
-                        <p>Query: {myContext.query}</p>
+                        {/* <p>Filter: {myContext.filter}</p>
+                        <p>Query: {myContext.query}</p> */}
+                        <div className="d-flex flex-row flex-wrap">
+                            {thumbnails.map((pdf) => (
+                                <PDFViewer thumbnail pdf={pdf} />
+                            ))}
+                        </div>
                     </div>
                 }
             />
